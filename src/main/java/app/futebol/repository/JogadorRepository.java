14e4b49208bd0ef218fc18d4e31339bdba1ac952
FabioFagundes01 <fabiogifagundes@gmail.com>
2025-05-26 15:26:34 -0300

    Delete src/main/java/app/futebol directory

--- src/main/java/app/futebol/repository/JogadorRepository.java
+++
@@ -1,9 +0,0 @@
-package app.futebol.repository; // Pacote atualizado
-
- import app.futebol.model.Jogador; // Import atualizado
- import org.springframework.data.jpa.repository.JpaRepository;
- import org.springframework.stereotype.Repository;
-
- @Repository
- public interface JogadorRepository extends JpaRepository<Jogador, Integer> {
- }
